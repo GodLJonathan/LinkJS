5263dda5f89aa730d8c56d9b08caaee89dc0ea83
GodLJonathan <91397110+GodLJonathan@users.noreply.github.com>
2021-10-14 14:50:15 +0530

    Add files via upload

---
+++ Models/Movie.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const movieSchema = new mongoose.Schema({
+
+    title: {type: String, required: true, unique: true},
+    description: {type: String, required: true},
+    bannerImage: {type: String},
+    trailer: {type: String},
+    posterImage: {type: String},
+    video: {type: String},
+    year: {type:String, required: true},
+    limit: {type: Number, required: true},
+    genre: {type: String, required: true},
+    category: {type: Array, required: true},
+
+},{timestamps:true});
+
+module.exports = mongoose.model('Movie', movieSchema);
---
+++ addLinks.js
@@ -0,0 +1,48 @@
+const { chromium } = require('playwright');
+const mongoose = require('mongoose');
+const Movie = require('./Models/Movie')
+
+require('dotenv').config();
+
+async function findTrailer(title) {
+    const browser = await chromium.launch({headless: false});
+    const page = await browser.newPage();
+    await page.goto('https://www.youtube.com/results?search_query=' + title + " trailer");
+    await page.click('#contents > .style-scope.ytd-item-section-renderer > #dismissible > .text-wrapper.style-scope.ytd-video-renderer > #meta > #title-wrapper > .title-and-badge.style-scope.ytd-video-renderer > #video-title')
+    const url = page.url()
+    await browser.close();
+    return url;
+}
+
+async function findAll() {
+    try {
+        const movies = await Movie.find({});
+        
+        for(const movie of movies) {
+            const trailerUrl = await findTrailer(movie.title);
+            movie.trailer = trailerUrl;
+            await movie.save();
+        }
+
+        console.log("All movies are done, stopping the code execution");
+        process.exit();
+
+    }
+    catch(err) {
+        console.error(err);
+    }
+}
+
+async function connectToMongoDB() {
+    try {
+        console.log("Trying to connect to MongoDB");
+        await mongoose.connect(process.env.MONGO_URL);
+        console.log("Connected to MongoDB");
+        findAll();
+    }
+    catch(err) {
+        console.error(err);
+    }
+}
+
+connectToMongoDB();
